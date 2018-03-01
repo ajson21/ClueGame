f3bc7789988ed9f80b8c4c32ea0a028a2a0a7103
ajson <ajson@mymail.mines.edu>
2018-03-01 15:45:00 -0700

    Finalized testing errors

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -4,20 +4,34 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 
 public class IntBoard {
+	
 	private HashMap<BoardCell, Set<BoardCell>> adjMtx;
-	private BoardCell[][] grid = new BoardCell[4][4];
+	private BoardCell[][] grid;
+	private Set<BoardCell> targets;
+	private Set<BoardCell> visited;
 
 	public IntBoard() {
 		super();
-		adjMtx = this.calcAdjacencies();
+		
+		grid = new BoardCell[4][4];
 		for(int i = 0; i < 4; i++){
+			
 			for(int j = 0; j < 4; j++){
-				BoardCell temp = new BoardCell(i,j);
-				grid[i][j] = temp;
+				
+				grid[i][j] = new BoardCell(i,j);
+				
 			}
+			
 		}
+		
+		adjMtx = new HashMap<BoardCell,Set<BoardCell>>();
+		adjMtx = calcAdjacencies();
+		
+		
+		
 	}
 	
 	public HashMap<BoardCell, Set<BoardCell>>calcAdjacencies(){
@@ -25,34 +39,52 @@ public class IntBoard {
 		HashMap<BoardCell, Set<BoardCell>> result = new HashMap<BoardCell, Set<BoardCell>>();
 		
 		for(int i = 0; i < 4; i++){
+			
 			for(int j = 0; j < 4; j++){
-				BoardCell current = new BoardCell(i,j);
-				Set<BoardCell> currentSet = new HashSet<BoardCell>();
 				
+				BoardCell current = grid[i][j];
+				System.out.println(current);
+				Set<BoardCell> currentSet = new HashSet<BoardCell>();
+	
 				for(int r = -1; r < 2; r+=2){
+					
 					if(!(current.getRow() + r == -1 || current.getRow() + r == 4)){
-						BoardCell temp = new BoardCell(current.getRow() + r, current.getColumn());
-						currentSet.add(temp);
+						
+						currentSet.add(new BoardCell(current.getRow() + r,current.getColumn()));
+						
 					}
+					
 				}
 				
 				for(int c = -1; c < 2; c+=2){
+					
 					if(!(current.getColumn() + c == -1 || current.getColumn() + c == 4)){
-						BoardCell temp = new BoardCell(current.getRow(), current.getColumn() + c);
-						currentSet.add(temp);
+						
+						currentSet.add(new BoardCell(current.getRow(),current.getColumn()+c));
+						
 					}
+					
 				}
 				
 				result.put(current,currentSet);
 				
 			}
+			
 		}
 		
 		return result;
 		
 	}
 	
-	public void getAdjList(){
+	public Set<BoardCell> getAdjList(BoardCell cell){
+		
+		Set<BoardCell> result = new HashSet<BoardCell>();
+		
+		result = adjMtx.get(cell);
+		
+		return result;
+		
+		
 		
 	}
 	
@@ -65,10 +97,9 @@ public class IntBoard {
 	}
 	
 	public BoardCell getCell(int row, int column){
+		
 		return grid[row][column];
+		
 	}
 	
-	public Set<BoardCell> getAdjList(BoardCell cell){
-		return adjMtx.get(cell);
-	}
 }
--- src/tests/IntBoardTests.java
+++ src/tests/IntBoardTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.Before;
@@ -10,66 +11,77 @@ import org.junit.Test;
 import experiment.BoardCell;
 import experiment.IntBoard;
 
-public class IntBoardTests{
+public class IntBoardTests {
 
 	IntBoard board;
-	
-	@Before 
-	public void beforeAll(){
+
+	@Before
+	public void beforeAll() {
+		
 		board = new IntBoard();
+		
 	}
-	
+
 	@Test
-	public void testAdjacencyLTC()
-	{
-		BoardCell cell = board.getCell(0,0);
-		Set<BoardCell> testList = board.getAdjList(cell);
+	public void testAdjacencyLTC() {
+		
+		BoardCell cell = board.getCell(0, 0);
+		
+		Set<BoardCell> testList = new HashSet<BoardCell>();
+		testList = board.getAdjList(cell);
 		assertTrue(testList.contains(board.getCell(1, 0)));
 		assertTrue(testList.contains(board.getCell(0, 1)));
 		assertEquals(2, testList.size());
+		
 	}
-	
+
 	@Test
-	public void testAdjacencyRBC()
-	{
-		BoardCell cell = board.getCell(3,3);
+	public void testAdjacencyRBC() {
+		
+		BoardCell cell = board.getCell(3, 3);
 		Set<BoardCell> testList = board.getAdjList(cell);
 		assertTrue(testList.contains(board.getCell(3, 2)));
 		assertTrue(testList.contains(board.getCell(2, 3)));
-		assertEquals(2,testList.size());
+		assertEquals(2, testList.size());
+		
 	}
+
 	@Test
-	public void testAdjacencyCP()
-	{
-		BoardCell cell = board.getCell(1,1);
+	public void testAdjacencyCP() {
+
+		BoardCell cell = board.getCell(1, 1);
 		Set<BoardCell> testList = board.getAdjList(cell);
+		System.out.println(testList);
 		assertTrue(testList.contains(board.getCell(0, 1)));
 		assertTrue(testList.contains(board.getCell(1, 0)));
 		assertTrue(testList.contains(board.getCell(1, 2)));
 		assertTrue(testList.contains(board.getCell(2, 1)));
 		assertEquals(2, testList.size());
+		
 	}
-	
+
 	@Test
-	public void testAdjacencyLE()
-	{
-		BoardCell cell = board.getCell(2,0);
+	public void testAdjacencyLE() {
+
+		BoardCell cell = board.getCell(2, 0);
 		Set<BoardCell> testList = board.getAdjList(cell);
 		assertTrue(testList.contains(board.getCell(1, 0)));
 		assertTrue(testList.contains(board.getCell(2, 1)));
 		assertTrue(testList.contains(board.getCell(3, 0)));
 		assertEquals(2, testList.size());
+		
 	}
-	
+
 	@Test
-	public void testAdjacencyRE()
-	{
-		BoardCell cell = board.getCell(2,3);
+	public void testAdjacencyRE() {
+		
+		BoardCell cell = board.getCell(2, 3);
 		Set<BoardCell> testList = board.getAdjList(cell);
 		assertTrue(testList.contains(board.getCell(3, 3)));
 		assertTrue(testList.contains(board.getCell(1, 3)));
 		assertTrue(testList.contains(board.getCell(2, 2)));
 		assertEquals(2, testList.size());
+		
 	}
-	
+
 }
