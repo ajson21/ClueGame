ac4985c8f773dda0afbe125c3682625b57ba35a3
ajson <ajson@mymail.mines.edu>
2018-04-03 16:04:29 -0600

    Created gameActionTests and skeleton for tests

---
+++ src/tests/gameActionTests.java
@@ -0,0 +1,14 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class gameActionTests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
